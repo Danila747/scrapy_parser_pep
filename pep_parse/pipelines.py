d5414ddabeb081ac17293e6c7e69a4dfd8012d2c
Daniel <105740743+Danila747@users.noreply.github.com>
2023-05-24 21:47:49 +0200

    Update pipelines.py

--- pep_parse/pipelines.py
+++ pep_parse/pipelines.py
@@ -7,14 +7,16 @@ BASE_DIR = settings.BASE_DIR
 BASE_DIR_1 = f"{BASE_DIR}/results/status_summary_"
 LIST = ('Статус', 'Количество')
 T = 'Total'
+DATEFORM = '%Y-%m-%d_%H-%M-%S'
 
 class PepParsePipeline:
     def open_spider(self, spider):
         self.statuses = collections.defaultdict(int)
 
     def close_spider(self, spider):
-        filename = f"{BASE_DIR_1}{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}.csv"
-        data = [LIST] + list(self.statuses.items()) + [(T, sum(self.statuses.values()))]
+        sum = sum(self.statuses.values())
+        filename = f"{BASE_DIR_1}{datetime.now().strftime(DATEFORM)}.csv"
+        data = [LIST] + list(self.statuses.items()) + [(T, sum)]
 
         with open(filename, mode='w', encoding='utf-8', newline='') as file:
             writer = csv.writer(file)
