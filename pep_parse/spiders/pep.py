1da8e1b43d89ead2947777bdc179de59b7f8f67b
Daniel <105740743+Danila747@users.noreply.github.com>
2023-05-24 21:36:41 +0200

    Update pep.py

--- pep_parse/spiders/pep.py
+++ pep_parse/spiders/pep.py
@@ -2,6 +2,9 @@ from scrapy.linkextractors import LinkExtractor
 from scrapy.spiders import CrawlSpider, Rule
 from pep_parse import items
 
+nummero = '//dt[contains(., "PEP")]/following-sibling::dd/text()'
+nammeno = '//*[@id="pep-content"]/h1/text()'
+statutello = '//dt[contains(., "Status")]/following-sibling::dd/text()'
 
 class PepSpider(CrawlSpider):
     name = 'pep'
@@ -17,13 +20,7 @@ class PepSpider(CrawlSpider):
 
     def parse_pep(self, response):
         return items.PepParseItem(
-            number=response.xpath(
-                                 '//dt[contains(., "PEP")]/following-sibling::dd/text()'
-                                 ).get().strip(),
-            name=response.xpath(
-                               '//*[@id="pep-content"]/h1/text()'
-                               ).get().strip(),
-            status=response.xpath(
-                                 '//dt[contains(., "Status")]/following-sibling::dd/text()'
-                                 ).get().strip()
+            number=response.xpath(nummero).get().strip(),
+            name=response.xpath(nammeno).get().strip(),
+            status=response.xpath(statutello).get().strip()
         )
